c64a9d4d0ebacf0f41e384daf265c63cd1d04bd3
rishab-partha <rishabparthasarathy@gmail.com>
2022-03-31 10:18:47 -0700

    Document

--- LeastSquares.py
+++ LeastSquares.py
@@ -1,33 +1,92 @@
+''' # This code establishes how to fit a function to a set of datapoints using a least-squares
+    # approach with gradient descent. 
+    #
+    # @author Rishab Parthasarathy
+    # @version 03.31.2022
+    #'''
+
 import argparse
-from pickle import NONE
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Define constants
 STEP_SIZE = 1e-7
 FPS_STEPS = np.array([-2*STEP_SIZE, -1*STEP_SIZE, STEP_SIZE, 2*STEP_SIZE])
 FPS_2D = np.reshape(FPS_STEPS, (1, 4))
 FPS_WEIGHTS = np.array([1.0, -8.0, 8.0, -1.0])
 
+''' # Method cubic defines a cubic function of the form 
+    #       f(x) = q_0 x^3 + q_1 x^2 + q_2 x + q_3.
+    #
+    # This method also processes a matrix of inputs and parameters, allowing
+    # multiple tests to be performed simultaneously.
+    # 
+    # @param inputs the matrix of input values to test
+    # @param q      the matrix of parameters to test
+    # @return       the matrix w/ values of f(x) for all sets of parameters/input
+    # '''
 def cubic(inputs, q):
     return q[0]*(inputs**3) + q[1]*(inputs**2) + q[2]*(inputs) + q[3]
     # End of method cubic
 
+''' # Method gaussian defines a gaussian function of the form 
+    #       f(x) = q_0^2 e^(-(x - q[1])^2/q[2]^2) + q[3]^2
+    #
+    # This method also processes a matrix of inputs and parameters, allowing
+    # multiple tests to be performed simultaneously.
+    # 
+    # @param inputs the matrix of input values to test
+    # @param q      the matrix of parameters to test
+    # @return       the matrix w/ values of f(x) for all sets of parameters/input
+    # '''
 def gaussian(inputs, q):
     return (q[0]**2)*np.exp(-1*((inputs-q[1])**2)/q[2]**2) + q[3]**2
     # End of method gaussian
 
+''' # Method sine defines a sine function of the form 
+    #       f(x) = q_0 sin(q[1] x + q[2]) + q[3].
+    #
+    # This method also processes a matrix of inputs and parameters, allowing
+    # multiple tests to be performed simultaneously.
+    # 
+    # @param inputs the matrix of input values to test
+    # @param q      the matrix of parameters to test
+    # @return       the matrix w/ values of f(x) for all sets of parameters/input
+    # '''
 def sine(inputs, q):
     return q[0]*np.sin(q[1]*inputs + q[2]) + q[3]
     # End of method sine
 
+''' # Method error calculates the error function:
+    #       Error = 0.5* sum((y - pred_y)^2)
+    # 
+    # @param out    the true values of y
+    # @param pred   predicted values of y
+    # @return       the value of the error fu
+    # '''
 def error(out, pred):
     return 0.5*np.sum((out - pred)**2)
     # End of method error
 
+''' # Method rmse calculates the root-mean-squared error function:
+    #       RMSE = sqrt(sum((y - pred_y)^2)/N)
+    # 
+    # @param out    the true values of y
+    # @param pred   predicted values of y
+    # @return       the value of the rms error function
+    # '''
 def rmse(out, pred):
     return np.sqrt(np.sum((out-pred)**2) / out.shape[0])
     # End of method rmse
 
+''' # Method partial calculates the partial derivatives relative to the parameters q using
+    # a matrix implementation of the five-point stencil.
+    # 
+    # @param inputs the input values to the function
+    # @param q      the current values of the parameters
+    # @param func   the function being fitted
+    # @return       the values of the partial derivatives relative to the parameters
+    # '''
 def partial(inputs, q, func):
     derivLocs = np.tile(q, (4*q.shape[0], 1)).T
     fps_tot = np.concatenate((np.pad(FPS_2D, ((0,3), (0,0))), np.pad(FPS_2D, ((1,2), (0,0))), 
@@ -39,6 +98,16 @@ def partial(inputs, q, func):
     return derivs # First dim is input loc, second dim is param #
     # End of method partial
 
+''' # Method gradient calculates the gradient relative to the parameters q using
+    # the partial derivatives and the current predictions.
+    # 
+    # @param inputs     the input values to the function
+    # @param outputs    the true output values
+    # @param q          the current values of the parameters
+    # @param func       the function being fitted
+    # @param lam        the learning factor
+    # @return           the value of the gradient relative to the parameters
+    # '''
 def gradient(inputs, outputs, q, func, lam):
     partialDerivs = partial(inputs, q, func)
     curPred = func(inputs, q)
@@ -47,6 +116,17 @@ def gradient(inputs, outputs, q, func, lam):
     return gradient
     # End of method gradient
 
+''' # Method oneIter makes one iteration of modifying the parameters and learning factors
+    # using the calculated gradients with gradient descent. If the error decreases, lambda
+    # is increased by 1.1; otherwise, lambda is halved.
+    # 
+    # @param inputs     the input values to the function
+    # @param outputs    the true output values
+    # @param q          the current values of the parameters
+    # @param func       the function being fitted
+    # @param lam        the learning factor
+    # @return           the new values of the learning factor and parameters
+    # '''
 def oneIter(inp, out, q, func, lam):
     pred = func(inp, q)
     error = rmse(out, pred)
@@ -63,6 +143,19 @@ def oneIter(inp, out, q, func, lam):
     return lam, error
     # End of method oneIter
 
+''' # Method train trains the least squares model for a given number of iterations or until
+    # an error threshold is hit, printing and updating errors/graphs each epoch.
+    # 
+    # @param inputs     the input values to the function
+    # @param outputs    the true output values
+    # @param q          the current values of the parameters
+    # @param func       the function being fitted
+    # @param lam        the learning factor
+    # @param thres      the error threshold
+    # @param maxIters   the maximum number of iterations
+    # @param epochIters the number of iterations in an epoch
+    # @postcondition    the least squares model has reached max iterations or the error threshold
+    # '''
 def train(inp, out, q, func, lam = 1e-1, thres = 1e-2, maxIters = 100000, epochIters = 1000):
     pred = func(inp, q)
     error = rmse(out, pred)
@@ -95,6 +188,11 @@ def train(inp, out, q, func, lam = 1e-1, thres = 1e-2, maxIters = 100000, epochI
     plt.show()
     # End of method train
 
+''' # Method readInput reads input data of with space separated values of x and y on each line.
+    # 
+    # @param inputFile  the input file path
+    # @return           the inputs/outputs read from the file
+    # '''
 def readInput(inputFile):
     fileInput = open(inputFile, 'r')
     allData = fileInput.readlines()
@@ -105,7 +203,13 @@ def readInput(inputFile):
     return inp, out
     # End of method readInput
 
-
+''' # Method readConfig reads the config file with the function type, the learning factor,
+    # error threshold, max iterations, initial parameters, and path of input file.
+    # 
+    # @param config     the config file path
+    # @return           the function type, learning factor, error threshold, max iterations,
+    #                   initial parameters, input/output data
+    # '''
 def readConfig(config):
     fileConfig = open(config, 'r')
     func = [str(val) for val in fileConfig.readline().split()][0]
@@ -115,6 +219,11 @@ def readConfig(config):
     return func, lam, thres, numIters, q, inp, out
     # End of method readConfig
 
+''' # Method main runs the least squares model on a given config file.
+    #
+    # @param config     the config file path
+    # @postcondition    the model has either run or has stated that the func was not recognized
+    # '''
 def main(config):
     func, lam, thres, numIters, q, inp, out = readConfig(config)
     if (func == 'cubic'):
@@ -126,6 +235,10 @@ def main(config):
     else:
         print("Function not recognized!")
     
+'''#
+   # This statement functions to run the main function by default. This statement also encodes
+   # a command line argument to accept the config file.
+   #'''
 if (__name__ == "__main__"):
     commandLineArgs = argparse.ArgumentParser()
     commandLineArgs.add_argument("--config", type = str, default = "configLeastSquares.txt")
